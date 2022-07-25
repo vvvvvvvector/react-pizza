ed7f92576b689d08ef1d3df1e6914706ffbbb9bf
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-07-25 17:14:12 +0300

    wrapper, header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import './scss/components/_header.scss'
+
+function Content() {
+  return (<h1>hello world!</h1>);
+}
+
 function App() {
   return (
-    <div>
-      <h1>hello world!!!</h1>
+    <div className="wrapper">
+      <div className="header">
+        <div className="container">
+          <div className="header__logo">
+            <img alt="store-logo" src={require("./assets/images/store-logo.jpg")} />
+            <div>
+              <h1>react pizza</h1>
+              <p>the best pizza in the universe</p>
+            </div>
+          </div>
+          here will be button
+        </div>
+      </div>
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.scss';
-
 import App from './App';
+
+import './scss/libs/app.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
