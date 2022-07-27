dad6813005ed73c9b68d30cada71a622602a09fc
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-07-27 16:30:39 +0300

    sort and pizza component done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logoSVG from './assets/images/store-logo.svg';
 import './scss/components/_header.scss';
 
@@ -10,11 +12,11 @@ import './scss/components/_categories.scss';
 import './scss/components/_sort.scss';
 import arrowSVG from './assets/images/categories-arrow.svg';
 
-function Content() {
-  return (<h1>hello world!</h1>);
-}
+import './scss/components/_pizza.scss';
 
 function App() {
+  const [showPopup, setShowPopup] = React.useState(false);
+
   return (
     <div className="wrapper">
       <div className="header">
@@ -57,41 +59,121 @@ function App() {
               <div className="sort__label">
                 <img alt="arrow" src={arrowSVG} />
                 <b>Sort by: </b>
-                <span>popularity (DESC)</span>
+                <span onClick={() => setShowPopup(!showPopup)}>popularity ↑</span>
               </div>
-              <div className="sort__popup">
-
-              </div>
+              {showPopup ? <div className="sort__popup">
+                <ul>
+                  <li className="active">popularity ↑</li>
+                  <li>popularity ↓</li>
+                  <li>cost ↑</li>
+                  <li>cost ↓</li>
+                  <li>alphabet ↑</li>
+                  <li>alphabet ↓</li>
+                </ul>
+              </div> : null}
             </div>
           </div>
           <h2 className="content__title">
             All pizzas
           </h2>
           <div className="content__items">
-
+            <div className="pizza-component">
+              <img className="pizza-component__image" alt="pizza-img" src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d48003cd-902c-420d-9f28-92d9dc5f73b4.jpg" />
+              <h4 className="pizza-component__name">Margherita</h4>
+              <div className="pizza-component__selector">
+                <ul>
+                  <li className="active">thin</li>
+                  <li>traditional</li>
+                </ul>
+                <ul>
+                  <li className="active">25 cm.</li>
+                  <li>30 cm.</li>
+                  <li>35 cm.</li>
+                </ul>
+              </div>
+              <div className="pizza-component__bottom">
+                <h4 className="pizza-component__price">from 7 $</h4>
+                <button className="button button--default">
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="#EB5A1E" />
+                  </svg>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
+            <div className="pizza-component">
+              <img className="pizza-component__image" alt="pizza-img" src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/ec29465e-606b-4a04-a03e-da3940d37e0e.jpg" />
+              <h4 className="pizza-component__name">Four Seasons</h4>
+              <div className="pizza-component__selector">
+                <ul>
+                  <li className="active">thin</li>
+                  <li>traditional</li>
+                </ul>
+                <ul>
+                  <li className="active">25 cm.</li>
+                  <li>30 cm.</li>
+                  <li>35 cm.</li>
+                </ul>
+              </div>
+              <div className="pizza-component__bottom">
+                <h4 className="pizza-component__price">from 9 $</h4>
+                <button className="button button--default">
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="#EB5A1E" />
+                  </svg>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
+            <div className="pizza-component">
+              <img className="pizza-component__image" alt="pizza-img" src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg" />
+              <h4 className="pizza-component__name">Pepperoni</h4>
+              <div className="pizza-component__selector">
+                <ul>
+                  <li className="active">thin</li>
+                  <li>traditional</li>
+                </ul>
+                <ul>
+                  <li className="active">25 cm.</li>
+                  <li>30 cm.</li>
+                  <li>35 cm.</li>
+                </ul>
+              </div>
+              <div className="pizza-component__bottom">
+                <h4 className="pizza-component__price">from 8 $</h4>
+                <button className="button button--default">
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="#EB5A1E" />
+                  </svg>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
+            <div className="pizza-component">
+              <img className="pizza-component__image" alt="pizza-img" src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg" />
+              <h4 className="pizza-component__name">Cheeseburger-pizza</h4>
+              <div className="pizza-component__selector">
+                <ul>
+                  <li className="active">thin</li>
+                  <li>traditional</li>
+                </ul>
+                <ul>
+                  <li className="active">25 cm.</li>
+                  <li>30 cm.</li>
+                  <li>35 cm.</li>
+                </ul>
+              </div>
+              <div className="pizza-component__bottom">
+                <h4 className="pizza-component__price">from 10 $</h4>
+                <button className="button button--default">
+                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="#EB5A1E" />
+                  </svg>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
           </div>
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
-          <Content />
         </div>
       </div>
     </div>
