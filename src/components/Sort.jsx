eb65af9defa47103019d45e3d587ce95ed4bdb26
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-07-28 16:21:00 +0300

    now you can select sort parameters

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 
 import arrowSVG from '../assets/images/categories-arrow.svg';
 
+const sortParameters = ["popularity ↑", "popularity ↓", "cost ↑", "cost ↓", "alphabet ↑", "alphabet ↓"];
+
 export function Sort() {
     const [showPopup, setShowPopup] = React.useState(false);
+    const [selectedSortParameter, setSelectedSortParameter] = React.useState(4);
 
     return (
         <div className="sort">
             <div className="sort__label">
                 <img alt="arrow" src={arrowSVG} />
                 <b>Sort by: </b>
-                <span onClick={() => setShowPopup(!showPopup)}>popularity ↑</span>
+                <span onClick={() => setShowPopup(!showPopup)}>{sortParameters[selectedSortParameter]}</span>
             </div>
             {showPopup ?
                 <div className="sort__popup">
                     <ul>
-                        <li className="active">popularity ↑</li>
-                        <li>popularity ↓</li>
-                        <li>cost ↑</li>
-                        <li>cost ↓</li>
-                        <li>alphabet ↑</li>
-                        <li>alphabet ↓</li>
+                        {
+                            sortParameters.map((parameter, id) => (
+                                <li onClick={() => setSelectedSortParameter(id)} key={id} className={id === selectedSortParameter ? "active" : null}>{parameter}</li>
+                            ))
+                        }
                     </ul>
                 </div> : null}
         </div>
