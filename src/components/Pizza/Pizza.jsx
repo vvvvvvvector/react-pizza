72238a882f13de042d4142c8b86696799f42f1ec
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-09-03 13:43:03 +0300

    pizza object in pizza instead of a lot parameters in Pizza func comp

--- src/components/Pizza/Pizza.jsx
+++ src/components/Pizza/Pizza.jsx
@@ -4,57 +4,55 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addPizza } from '../../redux/slices/cartSlice';
 import { setOpened, setPizza } from '../../redux/slices/overlaySlice';
 
-export const Pizza = ({ id, types, sizes, name, cost, imageURL, weight, diameter, description }) => {
+export const Pizza = (pizza) => {
     const dispatch = useDispatch();
 
     const [selectedType, setSelectedType] = React.useState(0);
     const [selectedSize, setSelectedSize] = React.useState(0);
 
-    const currentPizza = useSelector((state) => state.cart.pizzas.find((obj) => obj.name === name && obj.type === types[selectedType] && obj.diameter === diameter[selectedSize]));
+    const currentPizza = useSelector((state) => state.cart.pizzas.find((obj) => obj.name === pizza.name && obj.type === pizza.types[selectedType] && obj.diameter === pizza.diameter[selectedSize]));
     const amount = currentPizza ? currentPizza.amount : 0;
-
-    const pizzaObj = { types, id, sizes, name, cost, imageURL, weight, diameter, description };
 
     const onClickAdd = () => {
         dispatch(addPizza({
-            id: id,
-            type: types[selectedType],
-            diameter: diameter[selectedSize],
-            name: name,
+            id: pizza.id,
+            type: pizza.types[selectedType],
+            diameter: pizza.diameter[selectedSize],
+            name: pizza.name,
             amount: 1,
-            cost: cost,
-            imageURL: imageURL
+            cost: pizza.cost,
+            imageURL: pizza.imageURL
         }));
     };
 
     const onClickImage = () => {
         dispatch(setOpened(true));
-        dispatch(setPizza(pizzaObj));
+        dispatch(setPizza(pizza));
         document.body.style.overflow = 'hidden';
     };
 
     return (
         <div className="pizza-component">
-            <img onClick={onClickImage} className="pizza-component__image" alt="pizza-img" src={imageURL} />
-            <h4 className="pizza-component__name">{name}</h4>
+            <img onClick={onClickImage} className="pizza-component__image" alt="pizza-img" src={pizza.imageURL} />
+            <h4 className="pizza-component__name">{pizza.name}</h4>
             <div className="pizza-component__selector">
                 <ul>
                     {
-                        types.map((type, index) => (
+                        pizza.types.map((type, index) => (
                             <li key={index} onClick={() => setSelectedType(index)} className={index === selectedType ? "active" : null}>{type}</li>
                         ))
                     }
                 </ul>
                 <ul>
                     {
-                        sizes.map((size, index) => (
+                        pizza.sizes.map((size, index) => (
                             <li key={index} onClick={() => setSelectedSize(index)} className={index === selectedSize ? "active" : null}>{size}</li>
                         ))
                     }
                 </ul>
             </div>
             <div className="pizza-component__bottom">
-                <h4 className="pizza-component__price">from {cost} $</h4>
+                <h4 className="pizza-component__price">from {pizza.cost} $</h4>
                 <button onClick={onClickAdd} className="button button--default">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="#EB5A1E" />
