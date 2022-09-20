bdcfd4d37b65f3ad375db095dbad375affa2521c
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-09-20 14:33:03 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
