ca6219469958a4a340a02146dc6aca1684679940
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-14 03:01:53 +0200

    ?

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,79 +10,90 @@ import { setCurrentPage } from '../redux/home/slice';
 import { fetchHomePizzas } from '../redux/fetch/slice';
 import { RequestParametersTypes } from '../redux/fetch/types';
 
-import { Overlay, Categories, Sort, Pizza, Skeleton, Pagination } from '../components';
+import {
+  Overlay,
+  Categories,
+  Sort,
+  Pizza,
+  Skeleton,
+  Pagination,
+} from '../components';
 
-const sortParameters: string[] = ["popularity", "popularity", "cost", "cost", "name", "name"];
+const sortParameters: string[] = [
+  'popularity',
+  'popularity',
+  'cost',
+  'cost',
+  'name',
+  'name',
+];
 
 export const Home: React.FC = () => {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const opened = useSelector(selectOpened);
+  const opened = useSelector(selectOpened);
 
-    const {
-        status,
-        homePizzas
-    } = useSelector(selectFetch);
+  const { status, homePizzas } = useSelector(selectFetch);
 
-    const {
-        categoryIndex,
-        sortParameterIndex,
-        categoryName,
-        currentPage,
-        searchValue
-    } = useSelector(selectHome);
+  const {
+    categoryIndex,
+    sortParameterIndex,
+    categoryName,
+    currentPage,
+    searchValue,
+  } = useSelector(selectHome);
 
-    React.useEffect(() => {
-        const request: RequestParametersTypes = {
-            currentPage: currentPage,
-            categoryIndex: categoryIndex,
-            sortParameterName: sortParameters[sortParameterIndex],
-            sortParameterIndex: sortParameterIndex
-        };
-
-        dispatch(fetchHomePizzas(request));
-
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-        
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [categoryIndex, sortParameterIndex, currentPage]);
-
-    const renderContentItems = () => {
-        const skeletons = [...new Array(4)].map((_, index) => (
-            <Skeleton key={index} />
-        ));
-
-        const filteredPizzas = homePizzas.filter((item) => (
-            item.name.toLowerCase().includes(searchValue.toLowerCase())
-        )).map((pizza) => (
-            <Pizza key={pizza.id} {...pizza} />
-        ));
-
-        return status === "pending" ? skeletons : filteredPizzas;
+  React.useEffect(() => {
+    const request: RequestParametersTypes = {
+      currentPage: currentPage,
+      categoryIndex: categoryIndex,
+      sortParameterName: sortParameters[sortParameterIndex],
+      sortParameterIndex: sortParameterIndex,
     };
 
-    const onChangePage = (page: number) => {
-        dispatch(setCurrentPage(page));
-    };
+    dispatch(fetchHomePizzas(request));
 
-    return (
-        <>
-            {opened && <Overlay />}
-            <div className="content__top">
-                <Categories />
-                <Sort />
-            </div>
-            <h2 className="content__title">
-                {searchValue ? `All search results for "${searchValue}"` : `${categoryName} pizzas`}
-            </h2>
-            <div className="content__items">
-                {renderContentItems()}
-            </div>
-            <Pagination pageIndex={currentPage} onChangePage={onChangePage} />
-        </>
-    );
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [categoryIndex, sortParameterIndex, currentPage]);
+
+  const renderContentItems = () => {
+    const skeletons = [...new Array(4)].map((_, index) => (
+      <Skeleton key={index} />
+    ));
+
+    const filteredPizzas = homePizzas
+      .filter((item) =>
+        item.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+      .map((pizza) => <Pizza key={pizza.id} {...pizza} />);
+
+    return status === 'pending' ? skeletons : filteredPizzas;
+  };
+
+  const onChangePage = (page: number) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  return (
+    <>
+      {opened && <Overlay />}
+      <div className='content__top'>
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className='content__title'>
+        {searchValue
+          ? `All search results for "${searchValue}"`
+          : `${categoryName} pizzas`}
+      </h2>
+      <div className='content__items'>{renderContentItems()}</div>
+      <Pagination pageIndex={currentPage} onChangePage={onChangePage} />
+    </>
+  );
 };
