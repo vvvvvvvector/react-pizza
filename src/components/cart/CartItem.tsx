375fcc24d70d2a686bef87ac2877aad46faed4e1
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-04-14 02:42:11 +0200

    hide-scrollbars

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,60 +1,108 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import { incrementAmount, decrementAmount, removePizza } from '../../redux/cart/slice';
+import {
+  incrementAmount,
+  decrementAmount,
+  removePizza,
+} from '../../redux/cart/slice';
 import { CartItemType, UniquePizzaType } from '../../redux/cart/types';
 
-export const CartItem: React.FC<CartItemType> = ({ name, type, diameter, cost, amount, imageURL }) => {
-    const dispatch = useDispatch();
+export const CartItem: React.FC<CartItemType> = ({
+  name,
+  type,
+  diameter,
+  cost,
+  amount,
+  imageURL,
+}) => {
+  const dispatch = useDispatch();
 
-    const thisPizza: UniquePizzaType = { name, type, diameter };
+  const thisPizza: UniquePizzaType = { name, type, diameter };
 
-    const onClickPlus = () => {
-        dispatch(incrementAmount(thisPizza));
-    };
+  const onClickPlus = () => {
+    dispatch(incrementAmount(thisPizza));
+  };
 
-    const onClickMinus = () => {
-        dispatch(decrementAmount(thisPizza));
-    };
+  const onClickMinus = () => {
+    dispatch(decrementAmount(thisPizza));
+  };
 
-    const onRemovePizza = () => {
-        if (window.confirm("Do you really want to remove this pizza from the cart?")) {
-            dispatch(removePizza(thisPizza));
-        }
-    };
+  const onRemovePizza = () => {
+    if (
+      window.confirm('Do you really want to remove this pizza from the cart?')
+    ) {
+      dispatch(removePizza(thisPizza));
+    }
+  };
 
-    return (
-        <div className="cart-item">
-            <img src={imageURL} alt="pizza" />
-            <div className="info">
-                <h3>
-                    {name}
-                </h3>
-                <p>{type} dough, {diameter} cm</p>
-            </div>
-            <div className="amount">
-                <button disabled={amount === 1} className={amount > 1 ? "active" : ""} onClick={onClickMinus}>
-                    <svg width="11" height="10" viewBox="0 -3 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4.04019 0.0399933H8.84019C9.37035 0.0399933 9.80019 0.469833 9.80019 0.999993C9.80019 1.53015 9.37035 1.95999 8.84019 1.95999H4.04019H1.1602C0.630035 1.95999 0.200195 1.53015 0.200195 0.999993C0.200195 0.469833 0.630035 0.0399933 1.1602 0.0399933H4.04019Z" fill="#FE5F1E" />
-                    </svg>
-                </button>
-                <b>{amount}</b>
-                <button disabled={amount >= 99} className={amount >= 99 ? "" : "active"} onClick={onClickPlus}>
-                    <svg width="11" height="11" viewBox="-1 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.84019 4.04001H5.96019V1.16001C5.96019 0.629852 5.53035 0.200012 5.00019 0.200012C4.47003 0.200012 4.04019 0.629852 4.04019 1.16001V4.04001H1.1602C0.630035 4.04001 0.200195 4.46985 0.200195 5.00001C0.200195 5.53017 0.630035 5.96001 1.1602 5.96001H4.04019V8.84001C4.04019 9.37017 4.47003 9.80001 5.00019 9.80001C5.53035 9.80001 5.96019 9.37017 5.96019 8.84001V5.96001H8.84019C9.37035 5.96001 9.80019 5.53017 9.80019 5.00001C9.80019 4.46985 9.37035 4.04001 8.84019 4.04001Z" fill="#EB5A1E" />
-                    </svg>
-                </button>
-            </div>
-            <div className="cost">
-                <b>{cost * amount} $</b>
-            </div>
-            <div className="remove">
-                <button onClick={onRemovePizza}>
-                    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.74791 6.95572L6.49931 4.70712L8.74791 2.45852C9.16184 2.04459 9.16184 1.37338 8.74791 0.959454C8.33398 0.545525 7.66277 0.545525 7.24884 0.959454L5.00024 3.20805L2.75164 0.959454C2.33771 0.545525 1.66651 0.545525 1.25258 0.959454C0.838648 1.37338 0.838648 2.04459 1.25258 2.45852L3.50118 4.70712L1.25258 6.95572C0.838649 7.36965 0.838649 8.04086 1.25258 8.45479C1.66651 8.86872 2.33772 8.86872 2.75164 8.45479L5.00024 6.20619L7.24884 8.45479C7.66277 8.86872 8.33398 8.86872 8.74791 8.45479C9.16184 8.04086 9.16184 7.36965 8.74791 6.95572Z" fill="#D0D0D0" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className='cart-item hide-scrollbars'>
+      <img src={imageURL} alt='pizza' />
+      <div className='info'>
+        <h3>{name}</h3>
+        <p>
+          {type} dough, {diameter} cm
+        </p>
+      </div>
+      <div className='amount'>
+        <button
+          disabled={amount === 1}
+          className={amount > 1 ? 'active' : ''}
+          onClick={onClickMinus}
+        >
+          <svg
+            width='11'
+            height='10'
+            viewBox='0 -3 10 10'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M4.04019 0.0399933H8.84019C9.37035 0.0399933 9.80019 0.469833 9.80019 0.999993C9.80019 1.53015 9.37035 1.95999 8.84019 1.95999H4.04019H1.1602C0.630035 1.95999 0.200195 1.53015 0.200195 0.999993C0.200195 0.469833 0.630035 0.0399933 1.1602 0.0399933H4.04019Z'
+              fill='#FE5F1E'
+            />
+          </svg>
+        </button>
+        <b>{amount}</b>
+        <button
+          disabled={amount >= 99}
+          className={amount >= 99 ? '' : 'active'}
+          onClick={onClickPlus}
+        >
+          <svg
+            width='11'
+            height='11'
+            viewBox='-1 0 12 11'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M8.84019 4.04001H5.96019V1.16001C5.96019 0.629852 5.53035 0.200012 5.00019 0.200012C4.47003 0.200012 4.04019 0.629852 4.04019 1.16001V4.04001H1.1602C0.630035 4.04001 0.200195 4.46985 0.200195 5.00001C0.200195 5.53017 0.630035 5.96001 1.1602 5.96001H4.04019V8.84001C4.04019 9.37017 4.47003 9.80001 5.00019 9.80001C5.53035 9.80001 5.96019 9.37017 5.96019 8.84001V5.96001H8.84019C9.37035 5.96001 9.80019 5.53017 9.80019 5.00001C9.80019 4.46985 9.37035 4.04001 8.84019 4.04001Z'
+              fill='#EB5A1E'
+            />
+          </svg>
+        </button>
+      </div>
+      <div className='cost'>
+        <b>{cost * amount} $</b>
+      </div>
+      <div className='remove'>
+        <button onClick={onRemovePizza}>
+          <svg
+            width='10'
+            height='10'
+            viewBox='0 0 10 10'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M8.74791 6.95572L6.49931 4.70712L8.74791 2.45852C9.16184 2.04459 9.16184 1.37338 8.74791 0.959454C8.33398 0.545525 7.66277 0.545525 7.24884 0.959454L5.00024 3.20805L2.75164 0.959454C2.33771 0.545525 1.66651 0.545525 1.25258 0.959454C0.838648 1.37338 0.838648 2.04459 1.25258 2.45852L3.50118 4.70712L1.25258 6.95572C0.838649 7.36965 0.838649 8.04086 1.25258 8.45479C1.66651 8.86872 2.33772 8.86872 2.75164 8.45479L5.00024 6.20619L7.24884 8.45479C7.66277 8.86872 8.33398 8.86872 8.74791 8.45479C9.16184 8.04086 9.16184 7.36965 8.74791 6.95572Z'
+              fill='#D0D0D0'
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 };
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -10,82 +10,119 @@ import { EmptyCart, CartItem } from '../components';
 import cartSVG from '../assets/images/order-pizza-cart-top.svg';
 
 const Cart: React.FC = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const {
-        pizzas,
-        orderTotal,
-        amountTotal
-    } = useSelector(selectCart);
+  const { pizzas, orderTotal, amountTotal } = useSelector(selectCart);
 
-    if (pizzas.length === 0) {
-        return <EmptyCart />;
+  if (pizzas.length === 0) {
+    return <EmptyCart />;
+  }
+
+  const onClearCart = () => {
+    if (window.confirm('Do you really want to clear the cart?')) {
+      dispatch(clearCart());
     }
+  };
 
-    const onClearCart = () => {
-        if (window.confirm("Do you really want to clear the cart?")) {
-            dispatch(clearCart());
-        }
-    }
-
-    return (
-        <div className="cart">
-            <div className="cart__top">
-                <div className="title">
-                    <img alt="cart-order-logo" src={cartSVG} />
-                    <h2>Cart</h2>
-                </div>
-                <div className="clear">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2.5 5H4.16667H17.5" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M6.66663 5.00001V3.33334C6.66663 2.89131 6.84222 2.46739 7.15478 2.15483C7.46734 1.84227 7.89127 1.66667 8.33329 1.66667H11.6666C12.1087 1.66667 12.5326 1.84227 12.8451 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3451 17.8452C15.0326 18.1577 14.6087 18.3333 14.1666 18.3333H5.83329C5.39127 18.3333 4.96734 18.1577 4.65478 17.8452C4.34222 17.5326 4.16663 17.1087 4.16663 16.6667V5.00001H15.8333Z" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M8.33337 9.16667V14.1667" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M11.6666 9.16667V14.1667" stroke="#B6B6B6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                    <span onClick={onClearCart}>Clear cart</span>
-                </div>
-            </div>
-            <div className="cart__items">
-                {
-                    pizzas.map((pizza, index) => (
-                        <CartItem
-                            key={index}
-                            name={pizza.name}
-                            type={pizza.type}
-                            diameter={pizza.diameter}
-                            cost={pizza.cost}
-                            amount={pizza.amount}
-                            imageURL={pizza.imageURL} />
-                    ))
-                }
-            </div>
-            <div className="cart__bottom">
-                <div className="summary">
-                    <span>
-                        Pizzas amount:
-                        <b>{amountTotal}</b>
-                    </span>
-                    <span>
-                        Order total:
-                        <b>{orderTotal} $</b>
-                    </span>
-                </div>
-                <div className="buttons">
-                    <Link to="/">
-                        <div className="button button-go-back">
-                            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            <span>Go back</span>
-                        </div>
-                    </Link>
-                    <div className="button button-make-order">
-                        <span>Make an order!</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='cart'>
+      <div className='cart__top'>
+        <div className='title'>
+          <img alt='cart-order-logo' src={cartSVG} />
+          <h2>Cart</h2>
         </div>
-    );
+        <div className='clear'>
+          <svg
+            width='20'
+            height='20'
+            viewBox='0 0 20 20'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M2.5 5H4.16667H17.5'
+              stroke='#B6B6B6'
+              strokeWidth='1.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <path
+              d='M6.66663 5.00001V3.33334C6.66663 2.89131 6.84222 2.46739 7.15478 2.15483C7.46734 1.84227 7.89127 1.66667 8.33329 1.66667H11.6666C12.1087 1.66667 12.5326 1.84227 12.8451 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3451 17.8452C15.0326 18.1577 14.6087 18.3333 14.1666 18.3333H5.83329C5.39127 18.3333 4.96734 18.1577 4.65478 17.8452C4.34222 17.5326 4.16663 17.1087 4.16663 16.6667V5.00001H15.8333Z'
+              stroke='#B6B6B6'
+              strokeWidth='1.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <path
+              d='M8.33337 9.16667V14.1667'
+              stroke='#B6B6B6'
+              strokeWidth='1.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <path
+              d='M11.6666 9.16667V14.1667'
+              stroke='#B6B6B6'
+              strokeWidth='1.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+          </svg>
+          <span onClick={onClearCart}>Clear cart</span>
+        </div>
+      </div>
+      <div className='cart__items hide-scrollbars'>
+        {pizzas.map((pizza, index) => (
+          <CartItem
+            key={index}
+            name={pizza.name}
+            type={pizza.type}
+            diameter={pizza.diameter}
+            cost={pizza.cost}
+            amount={pizza.amount}
+            imageURL={pizza.imageURL}
+          />
+        ))}
+      </div>
+      <div className='cart__bottom'>
+        <div className='summary'>
+          <span>
+            Pizzas amount:
+            <b>{amountTotal}</b>
+          </span>
+          <span>
+            Order total:
+            <b>{orderTotal} $</b>
+          </span>
+        </div>
+        <div className='buttons'>
+          <Link to='/'>
+            <div className='button button-go-back'>
+              <svg
+                width='8'
+                height='14'
+                viewBox='0 0 8 14'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M7 13L1 6.93015L6.86175 1'
+                  stroke='#D3D3D3'
+                  strokeWidth='1.5'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+              <span>Go back</span>
+            </div>
+          </Link>
+          <div className='button button-make-order'>
+            <span>Make an order!</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
