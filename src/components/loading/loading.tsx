22ac57ad77b09297913f4631af6f768f74adb5d0
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-01-15 01:36:01 +0300

    new lazy loading fallback

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,7 +1,15 @@
+import { Loader } from 'lucide-react';
+
 export const Loading = ({ text }: { text: string }) => {
   return (
     <div className='loading-wrapper'>
       <h2>{text}</h2>
+      <Loader
+        className='animate-spin'
+        size={50}
+        color='#bfbfbf'
+        strokeWidth={1.5}
+      />
     </div>
   );
 };
