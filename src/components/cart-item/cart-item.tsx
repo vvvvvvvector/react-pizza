2675cc037d92daf284a4c58f2be0f5207b1323f4
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-01-15 18:25:54 +0300

    ver 0.1 done

--- src/components/cart-item/cart-item.tsx
+++ src/components/cart-item/cart-item.tsx
@@ -19,7 +19,24 @@ export const CartItem = ({
 
   const thisPizza: UniquePizza = { name, type, diameter };
 
-  const RemoveFromCart = () => (
+  const Image = () => <img src={imageURL} alt='pizza' />;
+
+  const Info = () => (
+    <div className='info hide-scrollbars'>
+      <h3 className='hide-scrollbars'>{name}</h3>
+      <p className='hide-scrollbars'>
+        {type} dough, {diameter} cm
+      </p>
+    </div>
+  );
+
+  const Cost = () => (
+    <div className='cost'>
+      <b>{cost * amount} $</b>
+    </div>
+  );
+
+  const RemoveFromCartButton = () => (
     <div className='remove'>
       <button
         onClick={() => {
@@ -48,73 +65,69 @@ export const CartItem = ({
     </div>
   );
 
+  const ChangeAmount = () => (
+    <div className='amount'>
+      <button
+        disabled={amount === 1}
+        className={amount > 1 ? 'active' : ''}
+        onClick={() => dispatch(decrementAmount(thisPizza))}
+      >
+        <svg
+          width='11'
+          height='10'
+          viewBox='0 -3 10 10'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M4.04019 0.0399933H8.84019C9.37035 0.0399933 9.80019 0.469833 9.80019 0.999993C9.80019 1.53015 9.37035 1.95999 8.84019 1.95999H4.04019H1.1602C0.630035 1.95999 0.200195 1.53015 0.200195 0.999993C0.200195 0.469833 0.630035 0.0399933 1.1602 0.0399933H4.04019Z'
+            fill='#FE5F1E'
+          />
+        </svg>
+      </button>
+      <b>{amount}</b>
+      <button
+        disabled={amount >= 99}
+        className={amount >= 99 ? '' : 'active'}
+        onClick={() => dispatch(incrementAmount(thisPizza))}
+      >
+        <svg
+          width='11'
+          height='11'
+          viewBox='-1 0 12 11'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M8.84019 4.04001H5.96019V1.16001C5.96019 0.629852 5.53035 0.200012 5.00019 0.200012C4.47003 0.200012 4.04019 0.629852 4.04019 1.16001V4.04001H1.1602C0.630035 4.04001 0.200195 4.46985 0.200195 5.00001C0.200195 5.53017 0.630035 5.96001 1.1602 5.96001H4.04019V8.84001C4.04019 9.37017 4.47003 9.80001 5.00019 9.80001C5.53035 9.80001 5.96019 9.37017 5.96019 8.84001V5.96001H8.84019C9.37035 5.96001 9.80019 5.53017 9.80019 5.00001C9.80019 4.46985 9.37035 4.04001 8.84019 4.04001Z'
+            fill='#EB5A1E'
+          />
+        </svg>
+      </button>
+    </div>
+  );
+
   return (
     <>
       <div className='cart-item mobile'>
         <div>
           <div>
-            <img src={imageURL} alt='pizza' />
-            <div className='info hide-scrollbars'>
-              <h3 className='hide-scrollbars'>{name}</h3>
-              <p className='hide-scrollbars'>
-                {type} dough, {diameter} cm
-              </p>
-            </div>
+            {Image()}
+            {Info()}
           </div>
-          {RemoveFromCart()}
+          {RemoveFromCartButton()}
+        </div>
+        <div>
+          {Cost()}
+          {ChangeAmount()}
         </div>
       </div>
       <div className='cart-item desktop hide-scrollbars'>
-        <img src={imageURL} alt='pizza' />
-        <div className='info hide-scrollbars'>
-          <h3 className='hide-scrollbars'>{name}</h3>
-          <p className='hide-scrollbars'>
-            {type} dough, {diameter} cm
-          </p>
-        </div>
-        <div className='amount'>
-          <button
-            disabled={amount === 1}
-            className={amount > 1 ? 'active' : ''}
-            onClick={() => dispatch(decrementAmount(thisPizza))}
-          >
-            <svg
-              width='11'
-              height='10'
-              viewBox='0 -3 10 10'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M4.04019 0.0399933H8.84019C9.37035 0.0399933 9.80019 0.469833 9.80019 0.999993C9.80019 1.53015 9.37035 1.95999 8.84019 1.95999H4.04019H1.1602C0.630035 1.95999 0.200195 1.53015 0.200195 0.999993C0.200195 0.469833 0.630035 0.0399933 1.1602 0.0399933H4.04019Z'
-                fill='#FE5F1E'
-              />
-            </svg>
-          </button>
-          <b>{amount}</b>
-          <button
-            disabled={amount >= 99}
-            className={amount >= 99 ? '' : 'active'}
-            onClick={() => dispatch(incrementAmount(thisPizza))}
-          >
-            <svg
-              width='11'
-              height='11'
-              viewBox='-1 0 12 11'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                d='M8.84019 4.04001H5.96019V1.16001C5.96019 0.629852 5.53035 0.200012 5.00019 0.200012C4.47003 0.200012 4.04019 0.629852 4.04019 1.16001V4.04001H1.1602C0.630035 4.04001 0.200195 4.46985 0.200195 5.00001C0.200195 5.53017 0.630035 5.96001 1.1602 5.96001H4.04019V8.84001C4.04019 9.37017 4.47003 9.80001 5.00019 9.80001C5.53035 9.80001 5.96019 9.37017 5.96019 8.84001V5.96001H8.84019C9.37035 5.96001 9.80019 5.53017 9.80019 5.00001C9.80019 4.46985 9.37035 4.04001 8.84019 4.04001Z'
-                fill='#EB5A1E'
-              />
-            </svg>
-          </button>
-        </div>
-        <div className='cost'>
-          <b>{cost * amount} $</b>
-        </div>
-        {RemoveFromCart()}
+        {Image()}
+        {Info()}
+        {ChangeAmount()}
+        {Cost()}
+        {RemoveFromCartButton()}
       </div>
     </>
   );
